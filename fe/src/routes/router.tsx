03afc27049e9066040f21eeb6e39d49101f0755c
mcuon <22130031@st.hcmuaf.edu.vn>
2025-12-28 21:35:13 +0700

    full front end register

--- fe/src/routes/router.tsx
+++ fe/src/routes/router.tsx
@@ -2,6 +2,7 @@ import { Layout } from "../layouts/layout";
 import HomePage from "../pages/home/Home";
 import Products from "../pages/products/product";
 import LoginPage from "../pages/login/login";
+import RegisterPage from "../pages/register/index";
 import { Route, Routes } from 'react-router-dom';
 const AppRoutes =() => {
     return (
@@ -9,6 +10,7 @@ const AppRoutes =() => {
             <Route element={<Layout />} >
                 <Route path="/" element={<HomePage />} />
                 <Route path="/login" element={<LoginPage />} />
+                <Route path='/register' element={<RegisterPage/>}/>
                 <Route path='/product' element={<Products/>}/>
             </Route>
             
