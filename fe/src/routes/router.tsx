47719bfba64f6c2b118de45892fd8e0880a37f04
mcuon <22130031@st.hcmuaf.edu.vn>
2026-01-07 15:30:33 +0700

    forgot password

--- fe/src/routes/router.tsx
+++ fe/src/routes/router.tsx
@@ -2,7 +2,9 @@ import { Layout } from "../layouts/layout";
 import HomePage from "../pages/home/Home";
 import Products from "../pages/products/product";
 import LoginPage from "../pages/login/login";
+import OAuth2RedirectHandler from '../features/auth/login/OAuth2RedirectHandler';
 import RegisterPage from "../pages/register/index";
+import ForgotPassword from "../features/auth/forgotPassword/ForgotPassword";
 import { Route, Routes } from 'react-router-dom';
 const AppRoutes =() => {
     return (
@@ -10,7 +12,9 @@ const AppRoutes =() => {
             <Route element={<Layout />} >
                 <Route path="/" element={<HomePage />} />
                 <Route path="/login" element={<LoginPage />} />
+                <Route path="/oauth2/redirect" element={<OAuth2RedirectHandler />} />
                 <Route path='/register' element={<RegisterPage/>}/>
+                <Route path="/forgot-password" element={<ForgotPassword />} />
                 <Route path='/product' element={<Products/>}/>
             </Route>
             
